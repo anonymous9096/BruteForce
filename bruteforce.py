694c516b1eaaa4cc4ea1ae362407e32ee20a9071
anonymous9096 <abirali20202@gmail.com>
2021-10-18 02:53:49 -0700

    brute

---
+++ bruteforce.py
@@ -0,0 +1,21 @@
+#brute_force
+
+import random
+import pyautogui
+
+all_input = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTTYUIOPASDFGHJKLZXCVBNM"
+
+all_input_list = list(all_input)
+
+password = pyautogui.password("Enter a password: ")
+
+guess_password = ""
+
+while guess_password != password:
+    guess_password = random.choices(all_input_list, k=len(password))
+
+    print("<================>"+ str(guess_password) +"<================>")
+
+    if guess_password == list(password):
+        print("Your Password is : "+ "".join(guess_password))
+        break
